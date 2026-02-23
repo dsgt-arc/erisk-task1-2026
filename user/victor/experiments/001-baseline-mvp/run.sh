0ebb3996d6e1c7a3e2c7445ec2343e831580e9ea
ViceBitz <25victorg@students.harker.org>
2026-02-22 22:14:55 -0500

    upload persona 0 run results

--- user/victor/experiments/001-baseline-mvp/run.sh
+++ user/victor/experiments/001-baseline-mvp/run.sh
@@ -6,7 +6,8 @@ salloc -N 1 --gres=gpu:V100:1 -t 2:00:00 --account=paceship-dsgt_clef2026
 
 #Set cache to scratch directory
 export HF_HOME=/storage/scratch1/8/vgong7/.cache/huggingface
-export XDG_CACHE_HOME=/storage/scratch1/8/vgong7/.cache
+export UV_CACHE_DIR=/storage/scratch1/8/vgong7/.cache/uv
+export UV_PYTHON_INSTALL_DIR=/storage/scratch1/8/vgong7/.local/share/uv/python
 
 #Pull latest code (if updates)
 cd scratch/erisk-2026
@@ -14,9 +15,9 @@ git pull origin main
 
 #Sync venv and packages
 export UV_PROJECT_ENVIRONMENT="/storage/scratch1/8/vgong7/.venv-erisk"
-cd scratch/erisk-2026...
-uv sync --package 001-baseline-mvp --python 3.11
 source /storage/scratch1/8/vgong7/.venv-erisk/bin/activate
+uv sync --package 001-baseline-mvp --python 3.11
+
 
 #Auth into HuggingFace
 hf auth login
